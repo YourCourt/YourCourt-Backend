d1b7062fedd47ec3ccfb1770916af006dfb4ed93
juanogtir <juanogtir@alum.us.es>
2021-02-25 12:24:07 +0100

    Configuración inicial
    Subida inicial del proyecto de Spring Boot

---
+++ yourcourt/src/main/java/yourcourt/YourcourtApplication.java
@@ -0,0 +1,13 @@
+package yourcourt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourcourtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourcourtApplication.class, args);
+	}
+
+}
---
+++ yourcourt/src/test/java/yourcourt/api/YourcourtApplicationTests.java
@@ -0,0 +1,13 @@
+package yourcourt.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourcourtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
