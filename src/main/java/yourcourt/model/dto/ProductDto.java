23be8d855f47f3d962aa54f080f0ce40d6d13a83
juanogtir <juanogtir@alum.us.es>
2021-04-26 17:03:38 +0200

    Controlador de Product
    Se han creado los métodos CRUD de la entidad Product.
    
    Tarea relacionada: #29

---
+++ src/main/java/yourcourt/model/dto/ProductDto.java
@@ -0,0 +1,26 @@
+package yourcourt.model.dto;
+
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import yourcourt.model.ProductType;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductDto{
+	
+	@NotBlank(message = "El nombre es obligatorio")
+	private String name;
+
+	@NotBlank(message = "La descripcion es obligatoria")
+	@Length(max = 512, message = "El tamaño del campo descripcion es demasiado largo, y el máximo son 512 caracteres.")
+	private String description;
+	
+	private ProductType productType;
+	
+}
--- src/main/java/yourcourt/service/ProductService.java
+++ src/main/java/yourcourt/service/ProductService.java
@@ -15,38 +15,52 @@
  */
 package yourcourt.service;
 
-import java.util.List;
-import java.util.Optional;
-
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+import yourcourt.exceptions.user.InexistentEntity;
+import yourcourt.model.News;
 import yourcourt.model.Product;
-import yourcourt.model.ProductType;
+import yourcourt.model.dto.ProductDto;
 import yourcourt.repository.ProductRepository;
-
 
 @Service
 public class ProductService {
-	
-	private ProductRepository productRepository;
+  private ProductRepository productRepository;
 
-	@Autowired
-	public ProductService(ProductRepository productRepository) {
-		this.productRepository = productRepository;
-	}
-	
-	@Transactional(readOnly = true)
-	public Optional<Product> findProductById(final Long id) throws DataAccessException {
-		return this.productRepository.findById(id);
-	}
-	
-	@Transactional
-	public void saveProduct(final Product product) throws DataAccessException {
-		this.productRepository.save(product);
-	}
-	
+  @Autowired
+  public ProductService(ProductRepository productRepository) {
+    this.productRepository = productRepository;
+  }
 
+  @Transactional(readOnly = true)
+  public Product findProductById(final Long id) throws DataAccessException {
+    return this.productRepository.findById(id)
+      .orElseThrow(() -> new InexistentEntity("Producto"));
+  }
+
+  public Iterable<Product> findAllProducts() {
+    return this.productRepository.findAll();
+  }
+
+  @Transactional
+  public Product saveProduct(final Product product) throws DataAccessException {
+    Product newProduct = this.productRepository.save(product);
+    return newProduct;
+  }
+
+  public Product updateProduct(Product productToUpdate, ProductDto productRequest) {
+    BeanUtils.copyProperties(productRequest, productToUpdate);
+    this.productRepository.save(productToUpdate);
+    return productToUpdate;
+  }
+
+  public void deleteProductById(Long id) {
+    Product product = productRepository
+      .findById(id)
+      .orElseThrow(() -> new InexistentEntity("Producto"));
+    productRepository.delete(product);
+  }
 }
